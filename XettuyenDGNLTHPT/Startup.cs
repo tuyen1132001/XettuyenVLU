22d386b99e8809f71e43ec191354899e031f62b7
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2022-06-21 21:47:44 +0700

    Start up Project

---
+++ XettuyenDGNLTHPT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XettuyenDGNLTHPT.Startup))]
+namespace XettuyenDGNLTHPT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
