92a2edf0c3798da5fe12323e8d5ca01d19b1098d
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2022-06-22 21:59:46 +0700

    Start chức năng xét tuyển THPTQG

--- XettuyenDGNLTHPT/Controllers/HomeController.cs
+++ XettuyenDGNLTHPT/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using XettuyenDGNLTHPT.Models;
 
 namespace XettuyenDGNLTHPT.Controllers
 {
     public class HomeController : Controller
     {
+        XettuyenVLUEntities model = new XettuyenVLUEntities();
         public ActionResult Index() //Form Dang ky THTP QG
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tblHoSoTHPT tblHoSoTHPT) //Form Dang ky THTP QG
+        {
+            if (ModelState.IsValid)
+            {
+                model.tblHoSoTHPTs.Add(tblHoSoTHPT);
+                model.SaveChanges();
+            }
             return View();
         }
 
