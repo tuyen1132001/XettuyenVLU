f7dc536dc1b1e44f37796a91369c1369764b5208
Tuyen <Tuyen@DESKTOP-2O0MUTK>
2022-07-07 20:17:51 +0700

    ViewFomrContent

---
+++ XettuyenDGNLTHPT/Areas/Admin/Controllers/FormContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XettuyenDGNLTHPT.Areas.Admin.Controllers
+{
+    public class FormContentController : Controller
+    {
+        // GET: Admin/FormContent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
