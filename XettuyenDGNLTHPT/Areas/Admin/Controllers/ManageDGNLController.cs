b38dbb65f812e9aa38e2a062b176782af6e469eb
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2022-07-12 21:51:55 +0700

    Lấy danh sách quản lý hồ sơ

---
+++ XettuyenDGNLTHPT/Areas/Admin/Controllers/ManageDGNLController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XettuyenDGNLTHPT.Models;
+
+namespace XettuyenDGNLTHPT.Areas.Admin.Controllers
+{
+    public class ManageDGNLController : Controller
+    {
+        SEP25Team08Entities model = new SEP25Team08Entities();
+        // GET: Admin/ManageDGNL
+        public ActionResult Index()
+        {
+            var list = model.tblHoSoDGNLs.ToList();
+            return View(list);
+        }
+
+        // GET: Admin/ManageDGNL/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/ManageDGNL/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageDGNL/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ManageDGNL/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageDGNL/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ManageDGNL/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageDGNL/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ XettuyenDGNLTHPT/Areas/Admin/Controllers/ManageTHPTController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XettuyenDGNLTHPT.Areas.Admin.Middleware;
+using XettuyenDGNLTHPT.Models;
+
+namespace XettuyenDGNLTHPT.Areas.Admin.Controllers
+{
+    public class ManageTHPTController : Controller
+    {
+        // GET: Admin/ManageTHPT
+        SEP25Team08Entities model = new SEP25Team08Entities();
+        public ActionResult Index()
+        {
+            var list = model.tblHoSoTHPTs.ToList();
+            return View(list);
+        }
+
+        // GET: Admin/ManageTHPT/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/ManageTHPT/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageTHPT/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ManageTHPT/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageTHPT/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/ManageTHPT/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/ManageTHPT/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
