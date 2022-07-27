d248d01968c869f06cf25aa7e70bd811e49a5704
Tuyen <Tuyen@DESKTOP-2O0MUTK>
2022-07-27 18:24:09 +0700

    View edit contentmail THPT/DGNL

---
+++ XettuyenDGNLTHPT/Areas/Admin/Controllers/ManageContentMailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XettuyenDGNLTHPT.Areas.Admin.Controllers
+{
+    public class ManageContentMailController : Controller
+    {
+        // GET: Admin/ManageContentMail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EditMailTHPT()
+        {
+            return View();
+        }
+        public ActionResult EditMailDGNL()
+        {
+            return View();
+        }
+
+    }
+}
